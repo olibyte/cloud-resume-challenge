76c015ff488f338657a73e976a1004bbca0729b0
Oliver <ocben1@gmail.com>
2021-11-21 19:21:12 +1100

    change lambda endpoints

--- cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
+++ cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
@@ -1,6 +1,6 @@
 
-fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Stage/put')
-    .then(() => fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Stage/get'))
+fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Prod/put')
+    .then(() => fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Prod/get'))
     .then(response => response.json())
     .then((data) => {
         document.getElementById('replaceme').innerText = data.count;
