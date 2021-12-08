f9a3616b1086b589a6c4d1ec9817b7515ecc0036
Oliver <ocben1@gmail.com>
2021-12-09 01:10:30 +1100

    git sync

--- cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
+++ cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
@@ -1,4 +1,3 @@
-
 fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Prod/put')
     .then(() => fetch('https://k07f479ka3.execute-api.us-east-1.amazonaws.com/Prod/get'))
     .then(response => response.json())
