81e18e1bb6b356f0d1e7ca819b4218066eda8af3
Oliver <ocben1@gmail.com>
2023-05-24 09:08:33 +1000

    change viewerProtocolPolicy

--- cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
+++ cloud-resume-challenge/resume-oliver-bennett/scripts/count.js
@@ -1,5 +1,5 @@
-fetch('https://d-jouiuecq10.execute-api.us-east-1.amazonaws.com/prod/put')
-    .then(() => fetch('https://d-jouiuecq10.execute-api.us-east-1.amazonaws.com/prod/get'))
+fetch('https://q0qfjomyb0.execute-api.us-east-1.amazonaws.com/prod/put')
+    .then(() => fetch('https://q0qfjomyb0.execute-api.us-east-1.amazonaws.com/prod/get'))
     .then(response => response.json())
     .then((data) => {
         document.getElementById('replaceme').innerText = data.count;
